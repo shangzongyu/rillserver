366645e46cdb733fa7b46dcb002e189c043c00cc
donney_luck <donney_luck@sina.cn>
2022-02-21 17:30:10 +0800

    fix pack.sh

--- release/pack.sh
+++ release/pack.sh
@@ -1,3 +1,11 @@
+###############################################################################
+# Copyright(C)   machine stdio                                                #
+# Author:        donney                                                       #
+# Email:         donney_luck@sina.cn                                          #
+# Date:          2022-02-21                                                   #
+# Description:   pack server                                                  #
+# Modification:  null                                                         #
+###############################################################################
 #!/bin/bash
 
 CUR_DIR=$(dirname $(readlink -f $0))
@@ -12,27 +20,25 @@ fi
 mkdir -p $DEST_DIR
 
 cd $SRC_DIR
-make rpc
 
 # Copy files
 FILES=(\
-  client      \
-  common      \
   luaclib     \
   lualib      \
-  lualib-src  \
-  Makefile    \
   proto       \
   service     \
-  web         \
-  sh          \
+  mod         \
+  preload     \
+  etc         \
+  config      \
+  start_node.sh
 )
 for file in ${FILES[*]} ; do
   cp -r $SRC_DIR/$file $DEST_DIR
 done
 
-mkdir -p $DEST_DIR/data
-cp $SRC_DIR/data/*.json $DEST_DIR/data
+#mkdir -p $DEST_DIR/data
+#cp $SRC_DIR/data/*.json $DEST_DIR/data
 
 SKYNET_DIR=$SRC_DIR/skynet
 SKYNET_DEST_DIR=$DEST_DIR/skynet
@@ -44,7 +50,8 @@ SKYNET_FILES=(\
   service  \
   skynet   \
 )
-for file in ${SKYNET_FILES[*]]} ; do
+
+for file in ${SKYNET_FILES[*]} ; do
   cp -r $SKYNET_DIR/$file $SKYNET_DEST_DIR
 done
 
@@ -53,11 +60,11 @@ mkdir -p $SKYNET_DEST_DIR/3rd/lua
 cp $SKYNET_DIR/3rd/lua/lua $SKYNET_DEST_DIR/3rd/lua/lua
 
 # Don't deploy setting file which is dependent on server.
-rm -f $DEST_DIR/common/settings.lua
-rm -f $DEST_DIR/common/clustername.lua
-rm -f $DEST_DIR/sh/server_dependency.sh
-rm -f $DEST_DIR/sh/debug*
-rm -f $DEST_DIR/sh/*.pid
+#rm -f $DEST_DIR/common/settings.lua
+#rm -f $DEST_DIR/common/clustername.lua
+#rm -f $DEST_DIR/sh/server_dependency.sh
+#rm -f $DEST_DIR/sh/debug*
+#rm -f $DEST_DIR/sh/*.pid
 
 #包名 fgame+时间+包类型
 DATE=$(date '+%Y-%m-%d_%H_%M_%S')
@@ -69,7 +76,7 @@ else
     mkdir -p $BUILD_DIR
 fi
 
-tar -czf $BUILD_DIR/hs_game-"$DATE".tar.gz *
+tar -czf $BUILD_DIR/game-"$DATE".tar.gz *
 
 rm -rf $DEST_DIR
 
