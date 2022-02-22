83b20f0f592e2456baa583bbc1abb18a500bfb51
donney_luck <donney_luck@sina.cn>
2022-02-22 15:24:00 +0800

    add usage for start_node.sh

--- start_node.sh
+++ start_node.sh
@@ -1,4 +1,16 @@
+###############################################################################
+# Copyright(C)   machine stdio                                                #
+# Author:        donney                                                       #
+# Email:         donney_luck@sina.cn                                          #
+# Date:          2022-02-22                                                   #
+# Description:   use to start server                                          #
+# Modification:  null                                                         #
+###############################################################################
 #!/bin/sh
+COLOR_RED='\033[31m'
+COLOR_GREEN='\033[32m'
+COLOR_RESET='\033[0m'
+
 export ROOT=$(cd `dirname $0`; pwd)
 export SKYNET_ROOT=$ROOT/skynet
 export DAEMON=false
@@ -20,5 +32,16 @@ done
 
 shift $[$OPTIND-1]
 
-$SKYNET_ROOT/skynet $ROOT/etc/config_node${1:-1}
+#$SKYNET_ROOT/skynet $ROOT/etc/config_node${1:-1}
+case "$1" in
+    1|2|3|4|5|6)
+    ;;
+	*)
+        echo -e "${COLOR_RED}Usage: $0 {-D node_id | -k | node_id}${COLOR_RESET}"
+        echo -e "${COLOR_GREEN}-D node_id  -run server daemon (need node_id) ${COLOR_RESET}"
+        echo -e "${COLOR_GREEN}-k          -kill server ${COLOR_RESET}"
+        echo -e "${COLOR_GREEN}node_id     -run server ${COLOR_RESET}"
+        exit 2
+esac
 
+$SKYNET_ROOT/skynet $ROOT/etc/config_node$1
